0c30604a459fa740277c102446ce43da8691c1f0
shijiaxin <shijiaxin@inspur.com>
2024-06-03 14:01:38 +0800

    测试2

--- src/libs/gpt.ts
+++ src/libs/gpt.ts
@@ -1,6 +1,6 @@
 import type { ChatMessage } from "@/types";
 
-export async function chat(messageList: ChatMessage[], apiKey: string) {
+export async function chat(messageList: ChatMessage[]) {
   try {
     const result = await fetch("https://emm-dev.inspuronline.com/ai/chat/stream", {
       method: "post",
@@ -8,7 +8,6 @@ export async function chat(messageList: ChatMessage[], apiKey: string) {
       // 开启后到达设定时间会中断流式输出
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
       },
       body: JSON.stringify({
         content: JSON.stringify(messageList)
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="flex flex-col h-screen">
-    <div
-      class="flex flex-nowrap fixed w-full items-baseline top-0 px-6 py-4 bg-gray-100"
-    >
-      <div class="text-2xl font-bold">ChatGPT</div>
-      <div class="ml-4 text-sm text-gray-500">
-        基于 OpenAI 的 ChatGPT 自然语言模型人工智能对话
-      </div>
-      <div
-        class="ml-auto px-3 py-2 text-sm cursor-pointer hover:bg-white rounded-md"
-        @click="clickConfig()"
-      >
-        设置
-      </div>
-    </div>
+<!--    <div-->
+<!--      class="flex flex-nowrap fixed w-full items-baseline top-0 px-6 py-4 bg-gray-100"-->
+<!--    >-->
+<!--      <div class="text-2xl font-bold">inGPT</div>-->
+<!--      <div class="ml-4 text-sm text-gray-500">-->
+<!--        inGPT人工智能对话-->
+<!--      </div>-->
+<!--    </div>-->
 
-    <div class="flex-1 mx-2 mt-20 mb-2" ref="chatListDom">
+    <div class="flex-1 mx-2 mt-2 mb-2" ref="chatListDom">
       <div
-        class="group flex flex-col px-4 py-3 hover:bg-slate-100 rounded-lg"
+        class="group flex  px-4 py-3 hover:bg-slate-100 rounded-lg  mt-1.5 "
+        :class="item.role=='assistant'?'justify-start':'justify-end'"
         v-for="item of messageList.filter((v) => v.role !== 'system')"
       >
-        <div class="flex justify-between items-center mb-2">
-          <div class="font-bold">{{ roleAlias[item.role] }}：</div>
-          <Copy class="invisible group-hover:visible" :content="item.content" />
-        </div>
-        <div>
+        <div class="bg-blue-500 rounded-3xl p-5">
           <div
-            class="prose text-sm text-slate-600 leading-relaxed"
+            class="prose text-sm text-white leading-relaxed"
             v-if="item.content"
             v-html="md.render(item.content)"
           ></div>
@@ -35,21 +26,20 @@
       </div>
     </div>
 
-    <div class="sticky bottom-0 w-full p-6 pb-8 bg-gray-100">
-      <div class="-mt-2 mb-2 text-sm text-gray-500" v-if="isConfig">
-        请输入 API Key：
-      </div>
-      <div class="flex">
-        <input
-          class="input"
-          :type="isConfig ? 'password' : 'text'"
-          :placeholder="isConfig ? 'sk-xxxxxxxxxx' : '请输入'"
+    <div class="sticky bottom-2 w-11/12 m-auto shadow-1xl rounded-3xl p-1 bg-gray-100">
+      <div class="flex flex-col justify-end">
+        <textarea
+          class="input rounded-2xl bg-transparent no-focus-shadow  outline-none border-0 resize-none"
+          :type="'text'"
+          :placeholder="'请输入'"
+
           v-model="messageContent"
-          @keydown.enter="isTalking || sendOrSave()"
+          @keydown.enter="isTalking || send()"
         />
-        <button class="btn" :disabled="isTalking" @click="sendOrSave()">
-          {{ isConfig ? "保存" : "发送" }}
+        <button class="btn self-end rounded-2xl text-xs h-8 w-20 flex justify-center items-center mr-1 " :disabled="isTalking" @click="send()">
+          {{"发送" }}
         </button>
+
       </div>
     </div>
   </div>
@@ -59,41 +49,24 @@
 import type { ChatMessage } from "@/types";
 import { ref, watch, nextTick, onMounted } from "vue";
 import { chat } from "@/libs/gpt";
-import cryptoJS from "crypto-js";
 import Loding from "@/components/Loding.vue";
 import Copy from "@/components/Copy.vue";
 import { md } from "@/libs/markdown";
 
-let apiKey = "222222";
-let isConfig = ref(true);
 let isTalking = ref(false);
 let messageContent = ref("");
 const chatListDom = ref<HTMLDivElement>();
 const decoder = new TextDecoder("utf-8");
-const roleAlias = { user: "ME", assistant: "ChatGPT", system: "System" };
+const roleAlias = { user: "用户", assistant: "小智", system: "System" };
 const messageList = ref<ChatMessage[]>([
   {
-    role: "system",
-    content: "你是 ChatGPT，OpenAI 训练的大型语言模型，尽可能简洁地回答。",
-  },
-  {
     role: "assistant",
-    content: `你好，我是AI语言模型，我可以提供一些常用服务和信息，例如：
-
-1. 翻译：我可以把中文翻译成英文，英文翻译成中文，还有其他一些语言翻译，比如法语、日语、西班牙语等。
-
-2. 咨询服务：如果你有任何问题需要咨询，例如健康、法律、投资等方面，我可以尽可能为你提供帮助。
-
-3. 闲聊：如果你感到寂寞或无聊，我们可以聊一些有趣的话题，以减轻你的压力。
-
+    content: `你好，我是inGPT语言模型，我可以提供一些常用服务和信息，例如：
 请告诉我你需要哪方面的帮助，我会根据你的需求给你提供相应的信息和建议。`,
   },
 ]);
 
 onMounted(() => {
-  if (getAPIKey()) {
-    switchConfigStatus();
-  }
 });
 
 const sendChatMessage = async (content: string = messageContent.value) => {
@@ -107,7 +80,7 @@ const sendChatMessage = async (content: string = messageContent.value) => {
     messageList.value.push({ role: "user", content });
     messageList.value.push({ role: "assistant", content: "" });
 
-    const { body, status } = await chat(messageList.value, getAPIKey());
+    const { body, status } = await chat(messageList.value);
     if (body) {
       const reader = body.getReader();
       await readStream(reader, status);
@@ -163,49 +136,14 @@ const readStream = async (
 const appendLastMessageContent = (content: string) =>
   (messageList.value[messageList.value.length - 1].content += content);
 
-const sendOrSave = () => {
+const send = () => {
   if (!messageContent.value.length) return;
-  if (isConfig.value) {
-    if (saveAPIKey(messageContent.value.trim())) {
-      switchConfigStatus();
-    }
-    clearMessageContent();
-  } else {
+
     sendChatMessage();
-  }
+
 };
 
-const clickConfig = () => {
-  if (!isConfig.value) {
-    messageContent.value = getAPIKey();
-  } else {
-    clearMessageContent();
-  }
-  switchConfigStatus();
-};
 
-const getSecretKey = () => "lianginx";
-
-const saveAPIKey = (apiKey: string) => {
-  if (apiKey.slice(0, 3) !== "sk-" || apiKey.length !== 51) {
-    alert("API Key 错误，请检查后重新输入！");
-    return false;
-  }
-  const aesAPIKey = cryptoJS.AES.encrypt(apiKey, getSecretKey()).toString();
-  localStorage.setItem("apiKey", aesAPIKey);
-  return true;
-};
-
-const getAPIKey = () => {
-  if (apiKey) return apiKey;
-  const aesAPIKey = localStorage.getItem("apiKey") ?? "";
-  apiKey = cryptoJS.AES.decrypt(aesAPIKey, getSecretKey()).toString(
-    cryptoJS.enc.Utf8
-  );
-  return apiKey;
-};
-
-const switchConfigStatus = () => (isConfig.value = !isConfig.value);
 
 const clearMessageContent = () => (messageContent.value = "");
 
@@ -225,4 +163,5 @@ pre {
     "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti",
     SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
 }
+
 </style>
